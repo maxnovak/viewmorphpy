cd395c3cec76f8eefaef7c9376dffd487be6089c
Paul Novak <paul@wingu.com>
2013-01-12 18:25:08 -0500

    add fundamental test

--- src/tests/test_fundamental.py
+++ src/tests/test_fundamental.py
@@ -4,6 +4,8 @@ from collections import namedtuple
 import Image
 
 
+import fundamental
+
 from nose.tools import assert_equal
 
 ImageTuple = namedtuple("Image", ['array', 'features'])
@@ -17,9 +19,19 @@ class TestFundamental(object):
     def load_image(self, image_file, feature_file):
         image = Image.open(image_file)
         array = numpy.array(image)
-        features = csv.reader(open(feature_file))
-        return ImageTuple(array, features)
+        features = []
+        for row in csv.reader(open(feature_file)):
+            features.append([float(r) for r in row])
+
+        return ImageTuple(array, numpy.mat(features))
 
     def test_image_array_shape(self):
-
         assert_equal((335, 500, 3), self.img0.array.shape)
+
+    def test_fundamental(self):
+        f = fundamental.fundamental(self.img0.features, self.img1.features)
+        expected = numpy.array([[  4.51920950e-09+0.j, 6.38289111e-10+0.j, -1.26394050e-05+0.j],
+                    [  1.89291668e-09+0.j, 1.09709829e-09+0.j, -9.60231967e-06+0.j],
+                    [ -2.65578925e-06+0.j, 5.38727389e-06+0.j, -2.24914918e-02+0.j]])
+        assert(numpy.allclose(expected, f))
+
